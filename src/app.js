9283e000a8d3454fcb041e1b1b20332fb5d53f74
znixxx30 <anshuman261001@gmail.com>
2026-02-22 14:57:24 +0530

    Setup Express server with base configuration

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "StockPilot API is running",
+    status: "success"
+  });
+});
+
+module.exports = app;
